0aff869215c03855fe455f253237234a54684996
Derek Choi <derekchoi105@gmail.com>
2016-09-06 16:51:23 +0530

    spring with web stuff

---
+++ DereksMavenProject/src/main/java/com/training/example/SimpleExamplePojo.java
@@ -0,0 +1,15 @@
+package com.training.example;
+
+public class SimpleExamplePojo {
+
+    private String hello;
+
+    public String getHello() {
+        return hello;
+    }
+
+    public void setHello(String hello) {
+        this.hello = hello;
+    }
+
+}
